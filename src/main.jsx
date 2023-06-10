f5e03833cc51c304fbb77fc287a9b81f0d288576
Royce Boone <royceboone@hotmail.com>
2023-06-10 10:37:02 -0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AppProvider } from "./context";
+import "./index.css";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AppProvider>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={true}></ReactQueryDevtools>
+    </QueryClientProvider>
+  </AppProvider>
+);
